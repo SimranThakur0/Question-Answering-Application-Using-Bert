ff9ad5ba7a0fd93e32759f7ac8dccac8cf375c1b
Simran thakur <shivangithakur7300@gmail.com>
2025-01-20 22:52:07 +0530

    data ingestion

--- src/Question_Answering_With_BERT/pipeline/data_ingestion_pipeline.py
+++ src/Question_Answering_With_BERT/pipeline/data_ingestion_pipeline.py
@@ -10,6 +10,11 @@ class DataIngestionTrainingPipeline:
     def main(self):
         config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
+        logger.info(f"Data ingestion config: {data_ingestion_config}")
+
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+        data_ingestion.validate_and_process_data()
+
+        logger.info("Data ingestion pipeline completed successfully!")
